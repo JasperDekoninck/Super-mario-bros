2b47135a23d76dbea1665554333b48d3d08ef0bb
Jasper Dekoninck <jasper.dekoninck@inf.ethz.ch>
2024-02-17 14:19:23 +0100

    init files

---
+++ src/mario/menus/__init__.py
@@ -0,0 +1,8 @@
+from .about_menu import *
+from .button import *
+from .game_over_menu import *
+from .level_creator_menu import *
+from .main_menu import *
+from .settings_menu import *
+from .play_menu import *
+
